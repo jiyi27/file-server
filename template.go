eb7a2226e873b3ea81ac83e49b921e79f13a7558
David Zhu <shaowenzhu@dal.ca>
2023-10-28 20:35:25 -0300

    feat: add share file feature

--- template.go
+++ template.go
@@ -12,6 +12,8 @@ import (
 
 type file struct {
 	Url         string
+	SharedUrl   string
+	IsDir       bool
 	CanDelete   bool
 	DisplayName template.HTML
 	DisplaySize template.HTML
@@ -42,16 +44,22 @@ func (s *server) getTemplateData(r *http.Request, files []os.FileInfo) templateD
 	}
 
 	for _, item := range files {
+		// init basic info of file entity.
 		filename := item.Name()
 		size := fileSizeBytes(item.Size()).String()
 		filePath := pwd + string(os.PathSeparator) + filename
 		canDelete := true
+		isDir := false
+		var sharedUrl string
 
+		// if file is directory, no shared url, no size info.
 		if item.IsDir() {
+			isDir = true
 			// the full path of dir has a separator "/" at the end: path/to/dir/
 			filename += string(os.PathSeparator)
 			filePath = pwd + string(os.PathSeparator) + filename
 			size = ""
+			sharedUrl = ""
 
 			// only empty dir can be deleted.
 			empty, err := s.isEmpty(filePath)
@@ -62,10 +70,22 @@ func (s *server) getTemplateData(r *http.Request, files []os.FileInfo) templateD
 			if !empty {
 				canDelete = false
 			}
+		} else {
+			// only files can be shared.
+			id, err := s.generateID(10)
+			if err != nil {
+				log.Printf(err.Error())
+			} else {
+				s.sharedFiles[id] = filePath
+				s.domain = strings.TrimSuffix(s.domain, `/`)
+				sharedUrl = fmt.Sprintf("%v%v?shared_id=%v", s.domain, filePath, id)
+			}
 		}
 
 		data.Files = append(data.Files, file{
 			Url:         filePath,
+			IsDir:       isDir,
+			SharedUrl:   sharedUrl,
 			CanDelete:   canDelete,
 			DisplayName: template.HTML(filename),
 			DisplaySize: template.HTML(size),
@@ -74,6 +94,22 @@ func (s *server) getTemplateData(r *http.Request, files []os.FileInfo) templateD
 	}
 
 	return data
+}
+
+func (s *server) generateID(n int) (string, error) {
+	for {
+		id, err := randomString(n)
+		if err != nil {
+			return "", fmt.Errorf("failed to generate file ID: %v", err)
+		}
+
+		_, ok := s.sharedFiles[id]
+		if ok {
+			continue
+		}
+
+		return id, err
+	}
 }
 
 type fileSizeBytes float64
