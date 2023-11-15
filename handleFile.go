e49e7c4708469228f821dea746eb47c3dd8127ea
David Zhu <shaowenzhu@dal.ca>
2023-11-15 19:03:59 -0400

    refactor: refine code

--- handleFile.go
+++ handleFile.go
@@ -42,6 +42,10 @@ func (s *server) handleDir(w http.ResponseWriter, r *http.Request, filePath stri
 	if err != nil {
 		return err
 	}
+
+	w.Header().Set("Connection", "Keep-Alive")
+	w.Header().Set("Keep-Alive", "timeout=2, max=1000")
+
 	return tmpl.Execute(w, data)
 }
 
