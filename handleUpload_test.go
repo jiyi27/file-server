b3eab82e6fc380cff3191655d16b0b3e3d4767c2
David Zhu <shaowenzhu@dal.ca>
2023-12-06 15:05:15 -0400

    test: add test for handleUpload function

---
+++ handleUpload_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"mime/multipart"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestHandleUpload_FileTooLarge(t *testing.T) {
+	s := &server{maxFileSize: 1} // 1 MB
+	currentDir := "./test_dir"
+	_ = os.Mkdir(currentDir, 0750)
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", "test.txt")
+	// Write more than 1MB of data
+	_, _ = io.Copy(part, bytes.NewBuffer(make([]byte, 2*1024*1024)))
+	_ = writer.Close()
+
+	req := httptest.NewRequest("POST", "/upload", body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	w := httptest.NewRecorder()
+
+	errs := s.handleUpload(w, req, currentDir)
+
+	if len(errs) == 0 || errs[0].Message != "copy file: http: request body too large" {
+		t.Errorf("Expected 'http: request body too large' error, got %v", errs[0].Message)
+	}
+
+	_, err := os.Stat(filepath.Join(currentDir, "test.txt"))
+	if !os.IsNotExist(err) { // new created file should be removed, because it's too large
+		t.Errorf("Expected file to not exist")
+	}
+
+	_ = os.RemoveAll(currentDir)
+}
